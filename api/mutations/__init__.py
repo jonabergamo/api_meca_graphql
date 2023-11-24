152bb53ecbacf8659570751f1267705751d9d96f
jonabergamo <jonathanbergamo16@gmail.com>
2023-11-24 00:54:37 -0300

    feat: jwt implementation

--- api/mutations/__init__.py
+++ api/mutations/__init__.py
@@ -1,3 +1,4 @@
 from .incubatorDevice import CreateIncubatorDevice, UpdateIncubatorDevice
 from .user import CreateUser, UpdateUser, DeleteUser
 from .incubatorSetting import CreateIncubatorSetting, UpdateIncubatorSetting, DeleteIncubatorSetting
+from .auth import ObtainJSONWebToken
