24f634fa0706ad88935c7a452cc0950567bd5209
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-12-12 14:37:05 -0300

    feat: added verification on query

--- api/querys/incubatorDeviceQuery.py
+++ api/querys/incubatorDeviceQuery.py
@@ -1,6 +1,7 @@
 import graphene
 from api.models import IncubatorDevice
 from api.types import IncubatorDeviceType
+from datetime import datetime, timezone
 
 
 class IncubatorDeviceQuery(graphene.ObjectType):
@@ -19,6 +20,16 @@ class IncubatorDeviceQuery(graphene.ObjectType):
         if not user.is_authenticated:
             raise Exception("Authentication credentials were not provided")
         try:
+            device = IncubatorDevice.objects.get(unique_id=unique_id)
+            if device.is_on and device.start_time and device.current_setting:
+                elapsed_time = datetime.now(timezone.utc) - device.start_time
+                incubation_duration = device.current_setting.incubation_duration
+                incubation_completed = elapsed_time.total_seconds() >= incubation_duration * 3600
+
+                if incubation_completed:
+                    device.is_on = False  # Desligar o dispositivo
+                    device.last_completion_data = datetime.now(timezone.utc)
+            device.save()
             return IncubatorDevice.objects.get(unique_id=unique_id)
         except IncubatorDevice.DoesNotExist:
             return None
